482607d8ffd0a72937bc445673fcff62ff96e58b
hkurth1 <hkurth1@smail.uni-koeln.de>
2022-01-09 12:21:36 +0100

    changes on Line 5 at branch_1

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
   
   public static void main(String[] args) {
     System.out.println("Code at line 4");
-    
+    System.out.println("Code at line 5 are also added in branch_1!");
+
   }
 }
